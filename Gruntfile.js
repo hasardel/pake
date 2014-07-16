62794346fd4f29fe16777456aadc48442cbc233c
pouthier <pouthier@i3s.unice.fr>
2014-07-16 17:13:27 +0200

    simplify package npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function (grunt) {
     'use strict';
 
     // load extern tasks
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-typescript');
 
     // tasks
@@ -17,19 +16,11 @@ module.exports = function (grunt) {
 				],
 				dest: 'lib/pake.js'
 			}
-		},
-        
-        uglify: {
-            build: {
-                files: [{
-                    'lib/pake.min.js': 'lib/pake.js'
-                }]
-            }
-        }
+		}
     });
 
     // register tasks
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build', ['typescript', 'uglify']);
+    grunt.registerTask('build', ['typescript']);
 }
 
